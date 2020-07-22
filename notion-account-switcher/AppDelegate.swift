024f0cfc4ee08ebba64e61c54459dc0d6c2000b1
Dora Lee <dsa28s@naver.com>
2020-07-23 00:57:00 +0900

    chore: Create macOS project

---
+++ notion-account-switcher/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  notion-account-switcher
+//
+//  Created by 도라도라 on 2020/07/23.
+//  Copyright © 2020 Dora Lee. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
