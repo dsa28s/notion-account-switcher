1a6b3a7b45b8a4e928ec87cc2ed80ae5c9cde3de
Dora Lee <dsa28s@naver.com>
2020-08-05 02:32:33 +0900

    chore(macOS): remove bundle file

--- NotionAccountSwitcherLDBServer/constant.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SERVER_PORT = void 0;
-exports.SERVER_PORT = 7555;
--- NotionAccountSwitcherLDBServer/error/notion-not-initialized-error.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const routing_controllers_1 = require("routing-controllers");
-class NotionNotInitializedError extends routing_controllers_1.HttpError {
-    constructor() {
-        super(403, 'Notion data not found. Please login first from notion app.');
-    }
-}
-exports.default = NotionNotInitializedError;
